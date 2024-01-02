97ecc5c0c4f8376525d6a00a31a07393a2c1237d
Noel Pena <127697218+noel-pena@users.noreply.github.com>
2024-01-02 11:50:10 -0500

    Delete src directory

--- src/components/theme.js
+++
@@ -1,9 +0,0 @@
-import { createTheme } from '@material-ui/core/styles';
-
-const theme = createTheme({
-    typography: {
-    fontFamily: "'Jost', sans-serif",
-    fontSize: "13px",
-    },
-});
-export default theme;
