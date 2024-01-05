c15316db6b39048eada38882cd36c85ddcd7eef4
noel-pena <azulu88@hotmail.com>
2024-01-05 10:54:45 -0500

    simon

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -5,9 +5,9 @@ import 'pure-react-carousel/dist/react-carousel.es.css';
 const Carousel = () => {
   return ( 
     <CarouselProvider
-      naturalSlideWidth={150}
-      naturalSlideHeight={150}
-      totalSlides={4}
+      naturalSlideWidth={125}
+      naturalSlideHeight={125}
+      totalSlides={5}
       infinite={true}
     >
       <Slider id='mySlider' >
@@ -17,11 +17,14 @@ const Carousel = () => {
       <a href='https://github.com/noel-pena/Random-Programmer-Quote-Generator'>
         <Slide className=" slide slide-2" index={1}>Random Programmer Quote</Slide>
       </a>
+      <a href='https://noel-pena.github.io/Simon-game-jQuery/'>
+        <Slide className=" slide slide-5" index={2}>Simon Game</Slide>
+      </a>
       <a href='https://noel-pena.github.io/Drums/'>
-        <Slide className="slide slide-3" index={2}>Drum App</Slide>
+        <Slide className="slide slide-3" index={3}>Drum App</Slide>
       </a>
       <a href='https://github.com/noel-pena'>
-        <Slide className="slide slide-4" index={3}>More projects on GitHub</Slide>
+        <Slide className="slide slide-4" index={4}>More projects on GitHub</Slide>
       </a>
       </Slider>
       <ButtonBack id="button">Back</ButtonBack>
