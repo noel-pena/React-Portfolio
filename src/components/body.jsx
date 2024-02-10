deacf90463d5b95c2a51aad04be1fad4b51dba39
noel-pena <azulu88@hotmail.com>
2024-02-10 17:37:51 -0500

    tipping app add

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -1,57 +1,79 @@
-import * as React from "react"
-import EmailIcon from '@mui/icons-material/Email';
-import { Grid } from '@mui/material';
-import Box from '@mui/material/Box';
-import Fab from '@mui/material/Fab';
-import { useNavigate } from 'react-router-dom';
+import * as React from "react";
+import EmailIcon from "@mui/icons-material/Email";
+import { Grid } from "@mui/material";
+import Box from "@mui/material/Box";
+import Fab from "@mui/material/Fab";
+import { useNavigate } from "react-router-dom";
 import Carousel from "./carousel";
 
-function Body(){
-    const navigate = useNavigate();
+function Body() {
+  const navigate = useNavigate();
 
-    const handleNav = () => {
-        navigate('/contact')
-    }
-    
+  const handleNav = () => {
+    navigate("/contact");
+  };
 
-    return(
+  return (
     <Grid container>
-        <div className="img" style={{ position: 'fixed', top: '83%', left: '2%'}}>
-            <Box sx={{ '& > :not(style)': { m: 1 } }}>
-                <Fab sx={{ color: 'gray' }} aria-label="add">
-                    <EmailIcon onClick={handleNav} />
-                </Fab>
-            </Box>
-            </div>
-        <Grid container className="container">
-            <Grid item xs={12} md={6}>
-                <img className="profile-pic" src="/img/IMG_0430.PNG" alt="profile picture"/>
-            </Grid>
-            <Grid item xs={12} md={6}>
-                <h1 >Javascript full-stack developer.</h1>
-                <p className="quote">"Hard work beats talent when talent doesn't work hard." - Tim Notke</p>
-            </Grid>
+      <div
+        className="img"
+        style={{ position: "fixed", top: "83%", left: "2%" }}
+      >
+        <Box sx={{ "& > :not(style)": { m: 1 } }}>
+          <Fab sx={{ color: "gray" }} aria-label="add">
+            <EmailIcon onClick={handleNav} />
+          </Fab>
+        </Box>
+      </div>
+      <Grid container className="container">
+        <Grid item xs={12} md={6}>
+          <img
+            className="profile-pic"
+            src="/img/IMG_0430.PNG"
+            alt="profile picture"
+          />
         </Grid>
-        <Grid xs={12} md={4}  className="container-2">
-                <h1 className="about">About Me</h1>
-            <div className="container-3">
-                <p>My name is Noel Pena and I am a web developer. I code mainly in React.js. On my free time, I am a photographer and also do a lot of photo editing. I am a music producer on the side where I primarily work with virtual and live instruments. If you're interested in my work, please feel free to contact me. You may also download a copy of my resume under the resume link.</p>
-            </div>
+        <Grid item xs={12} md={6}>
+          <h1>Full-stack developer.</h1>
+          <p className="quote">
+            "Hard work beats talent when talent doesn't work hard." - Tim Notke
+          </p>
         </Grid>
-        <Grid xs={12} md={4} className="container-4">
-                <h1 className="about">Skills</h1>
-            <div className="container-5">
-                <p >HTML • CSS • Flexbox • Grid • Bootstrap • Javascript • DOM Manipulation • jQuery • Bash Command Line • Git, GitHub and Version Control • Backend Web Development • Node.js • NPM • Express.js • EJS • REST • APIs • Databases • SQL • PostgreSQL • Mysql • Authentication • oauth • Password hashing • React.js • Web Design • Deployment with GitHub Pages • MUI React </p>
-            </div>
-        </Grid>
-        <Grid xs={12} md={4} className="container-2">
-                <h1 className="about">Portfolio</h1>
-            <div className="container-3">
-                <Carousel />
-            </div>
-        </Grid>
-    </Grid>  
-    )
+      </Grid>
+      <Grid xs={12} md={4} className="container-2">
+        <h1 className="about">About Me</h1>
+        <div className="container-3">
+          <p>
+            My name is Noel Pena and I am a web developer. I code mainly in
+            React.js. On my free time, I am a photographer and also do a lot of
+            photo editing. I am a music producer on the side where I primarily
+            work with virtual and live instruments. If you're interested in my
+            work, please feel free to contact me. You may also download a copy
+            of my resume under the resume link.
+          </p>
+        </div>
+      </Grid>
+      <Grid xs={12} md={4} className="container-4">
+        <h1 className="about">Skills</h1>
+        <div className="container-5">
+          <p>
+            HTML • CSS • Flexbox • Grid • Bootstrap • Javascript • DOM
+            Manipulation • jQuery • Bash Command Line • Git, GitHub and Version
+            Control • Backend Web Development • Node.js • NPM • Express.js • EJS
+            • REST • APIs • Databases • SQL • PostgreSQL • Mysql •
+            Authentication • oauth • Password hashing • React.js • Web Design •
+            Deployment with GitHub Pages • MUI React{" "}
+          </p>
+        </div>
+      </Grid>
+      <Grid xs={12} md={4} className="container-2">
+        <h1 className="about">Portfolio</h1>
+        <div className="container-3">
+          <Carousel />
+        </div>
+      </Grid>
+    </Grid>
+  );
 }
 
-export default Body
+export default Body;
--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -1,34 +1,57 @@
-import React from 'react';
-import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
-import 'pure-react-carousel/dist/react-carousel.es.css';
+import React from "react";
+import {
+  CarouselProvider,
+  Slider,
+  Slide,
+  ButtonBack,
+  ButtonNext,
+} from "pure-react-carousel";
+import "pure-react-carousel/dist/react-carousel.es.css";
 
 const Carousel = () => {
-  return ( 
+  return (
     <CarouselProvider
       naturalSlideWidth={125}
       naturalSlideHeight={125}
-      totalSlides={6}
+      totalSlides={7}
       infinite={true}
     >
-      <Slider id='mySlider' >
-      <a href="https://github.com/noel-pena/Todo-List">
-        <Slide className="slide slide-1"  index={0}>To-Do List</Slide>
-      </a>
-      <a href='https://github.com/noel-pena/Random-Programmer-Quote-Generator'>
-        <Slide className=" slide slide-2" index={1}>Random Programmer Quote</Slide>
-      </a>
-      <a href='https://noel-pena.github.io/Simon-game-jQuery/'>
-        <Slide className=" slide slide-5" index={2}>Simon Game</Slide>
-      </a>
-      <a href='https://noel-pena.github.io/Drums/'>
-        <Slide className="slide slide-3" index={3}>Drum App</Slide>
-      </a>
-      <a href='https://github.com/noel-pena/Calculator'>
-        <Slide className="slide slide-6" index={4}>Calculator</Slide>
-      </a>
-      <a href='https://github.com/noel-pena'>
-        <Slide className="slide slide-4" index={5}>More projects on GitHub</Slide>
-      </a>
+      <Slider id="mySlider">
+        <a href="https://github.com/noel-pena/Tipping-app">
+          <Slide className="slide slide-7" index={5}>
+            Tipping App
+          </Slide>
+        </a>
+        <a href="https://github.com/noel-pena/Todo-List">
+          <Slide className="slide slide-1" index={0}>
+            To-Do List
+          </Slide>
+        </a>
+        <a href="https://github.com/noel-pena/Random-Programmer-Quote-Generator">
+          <Slide className=" slide slide-2" index={1}>
+            Random Programmer Quote
+          </Slide>
+        </a>
+        <a href="https://noel-pena.github.io/Simon-game-jQuery/">
+          <Slide className=" slide slide-5" index={2}>
+            Simon Game
+          </Slide>
+        </a>
+        <a href="https://noel-pena.github.io/Drums/">
+          <Slide className="slide slide-3" index={3}>
+            Drum App
+          </Slide>
+        </a>
+        <a href="https://github.com/noel-pena/Calculator">
+          <Slide className="slide slide-6" index={4}>
+            Calculator
+          </Slide>
+        </a>
+        <a href="https://github.com/noel-pena">
+          <Slide className="slide slide-4" index={6}>
+            More projects on GitHub
+          </Slide>
+        </a>
       </Slider>
       <ButtonBack id="button">Back</ButtonBack>
       <ButtonNext id="button">Next</ButtonNext>
