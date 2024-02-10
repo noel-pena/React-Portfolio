40f43d6502438e2e9e0bd5ffba132eb54b8e15ed
noel-pena <azulu88@hotmail.com>
2024-02-10 18:23:54 -0500

    s3 fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import './App.css'
-import { Home } from './pages/Home'
-import { Contact } from './pages/Contact'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import "../public/styles.css";
+import { Home } from "./pages/Home";
+import { Contact } from "./pages/Contact";
 
 function App() {
   return (
     <>
-    <Router>
-
-    <Routes>
-      <Route exact path='/' element={<Home/>}/>
-      <Route path='/contact' element={<Contact/>}/>
-    </Routes>
-    </Router>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
